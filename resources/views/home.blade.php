f2603b518a8225c4f27fc5634a342c704f7919c5
kurob <kurob1993@gmail.com>
2020-03-13 07:55:40 +0700

    usage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,5 +18,25 @@
             </div>
         </div>
     </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8 mt-3">
+            <div class="card">
+                <div class="card-header">USAGE</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <code>
+                        curl --location --request POST 'https://pwm.kurob.web.id/api/v1/message/store?number=628992141874&text=TEST%20API%20WA%203' \
+                        --header 'Accept: application/json' \
+                        --header 'Authorization: Bearer **api key** '
+                    </code>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
