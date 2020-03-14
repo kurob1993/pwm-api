3e16b494d60664e7a6501fffc40efb618a2de363
kurob <kurob1993@gmail.com>
2020-03-14 11:18:20 +0700

    perbaikan penempatan alert session

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">API KEY</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
                     <code>{{ $user->api_token }}</code>
                 </div>
             </div>
@@ -24,11 +24,6 @@
                 <div class="card-header">USAGE</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
                     <code>
                         curl --location --request POST 'https://pwm.kurob.web.id/api/v1/message/store?number=628992141874&text=TEST%20API%20WA%203' \
                         --header 'Accept: application/json' \
